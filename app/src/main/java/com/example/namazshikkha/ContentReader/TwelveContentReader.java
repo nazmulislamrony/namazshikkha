7f626eb00a1ccad85498c9201bbd396e9ed64aed
nazmulisrony <nazmulisrony@gmail.com>
2020-05-11 14:10:34 +0600

    image add

--- app/src/main/java/com/example/namazshikkha/ContentReader/TwelveContentReader.java
+++ app/src/main/java/com/example/namazshikkha/ContentReader/TwelveContentReader.java
@@ -16,17 +16,32 @@ import com.example.namazshikkha.R;
 public class TwelveContentReader extends AppCompatActivity {
 
 
-    ImageView twelveImg;
+    ImageView first, second, third, fourth, fifth, sixth, seventh, eight, nine, tenth, eleventh, twelve, thirteen, fourtheen, fifteen, sixteen, seventeen;
     TwelveStore twelveStore;
-    LinearLayout lineLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twelve_content_reader);
 
-        twelveImg=findViewById(R.id.twelveImage);
-        lineLayout=findViewById(R.id.imgeLinear);
+        first=findViewById(R.id.firstTwelve);
+        second=findViewById(R.id.secondTwelve);
+        third=findViewById(R.id.thirdTwelve);
+        fourth=findViewById(R.id.fourthTwelve);
+        fifth=findViewById(R.id.fifthTwelve);
+        sixth=findViewById(R.id.sixthTwelve);
+        seventh=findViewById(R.id.seventhTwelve);
+        eight=findViewById(R.id.eightTwelve);
+        nine=findViewById(R.id.ninthTwelve);
+        tenth=findViewById(R.id.tenthTwelve);
+        eleventh=findViewById(R.id.elevenTwelve);
+        twelve=findViewById(R.id.twelveTwelve);
+        thirteen=findViewById(R.id.thirteenTwelve);
+        fourtheen=findViewById(R.id.fourteenTwelve);
+        fifteen=findViewById(R.id.fiftheenTwelve);
+        sixteen=findViewById(R.id.sixteenTwelve);
+        seventeen=findViewById(R.id.seventeenTwelve);
+
 
         twelveStore=getIntent().getParcelableExtra("TWELVE");
 
@@ -36,11 +51,29 @@ public class TwelveContentReader extends AppCompatActivity {
 
 
         if (twelveStore.getId()==1){
-            twelveImg.setImageResource(R.drawable.comfort);
-            twelveImg.setImageResource(R.drawable.comfort);
-            twelveImg.setImageResource(R.drawable.finance);
+            first.setImageResource(R.drawable.first);
+            second.setImageResource(R.drawable.second);
+            third.setImageResource(R.drawable.third);
+            fourth.setImageResource(R.drawable.fourth);
+            fifth.setImageResource(R.drawable.fifth);
+
+            sixth.setImageResource(R.drawable.six);
+            seventh.setImageResource(R.drawable.second);
+            eight.setImageResource(R.drawable.eight);
+            nine.setImageResource(R.drawable.nine);
+            tenth.setImageResource(R.drawable.ten);
+
+            eleventh.setImageResource(R.drawable.eleven);
+            twelve.setImageResource(R.drawable.twelve);
+            thirteen.setImageResource(R.drawable.thirteen);
+            fourtheen.setImageResource(R.drawable.fourtenn);
+            fifteen.setImageResource(R.drawable.fiftenn);
+
+            sixteen.setImageResource(R.drawable.sixteen);
+            seventeen.setImageResource(R.drawable.seventeen);
+
         }else {
-            twelveImg.setImageResource(R.drawable.education);
+            fourth.setImageResource(R.drawable.education);
         }
     }
 }
