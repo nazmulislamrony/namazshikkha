a1b54daaf0550e55520b690cc88dfc51745b8b89
nazmulisrony <nazmulisrony@gmail.com>
2020-05-13 02:48:40 +0600

    design part change

--- app/src/main/java/com/example/namazshikkha/SplashScreen.java
+++ app/src/main/java/com/example/namazshikkha/SplashScreen.java
@@ -1,8 +1,10 @@
 package com.example.namazshikkha;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -19,11 +21,11 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_splash_screen);
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
 
         thread=new Thread(new Runnable() {
             @Override
@@ -38,7 +40,7 @@ public class SplashScreen extends AppCompatActivity {
 
     private void doWork() {
             try {
-                Thread.sleep(550);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
