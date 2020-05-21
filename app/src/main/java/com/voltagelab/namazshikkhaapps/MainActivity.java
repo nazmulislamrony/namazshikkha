bf52b49355442d230e66f40f1ba8c704a327e7d3
nazmulisrony <nazmulisrony@gmail.com>
2020-05-21 14:13:02 +0600

    final first version app

--- app/src/main/java/com/voltagelab/namazshikkhaapps/MainActivity.java
+++ app/src/main/java/com/voltagelab/namazshikkhaapps/MainActivity.java
@@ -212,18 +212,14 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case R.id.rating:
-                        Uri uri = Uri.parse("market://details?id=com.voltagelab.namazshikkhaapps" + getBaseContext().getPackageName());
-                        Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
-                        // To count with Play market backstack, After pressing back button,
-                        // to taken back to our application, we need to add following flags to intent.
-                        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY |
-                                Intent.FLAG_ACTIVITY_NEW_DOCUMENT |
-                                Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
+
                         try {
-                            startActivity(goToMarket);
-                        } catch (ActivityNotFoundException e) {
                             startActivity(new Intent(Intent.ACTION_VIEW,
-                                    Uri.parse("http://play.google.com/store/apps/details?id=com.voltagelab.namazshikkhaapps" + getBaseContext().getPackageName())));
+                                    Uri.parse("market://details?id="+getPackageName())
+                                    ));
+                        }catch (ActivityNotFoundException e){
+                            startActivity(new Intent(Intent.ACTION_VIEW,
+                                    Uri.parse("https://play.google.com/store/apps/details?id="+getPackageName())));
                         }
                         break;
 
